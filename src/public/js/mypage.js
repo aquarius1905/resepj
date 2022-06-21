17e857545744b7d6d5767770f3511284b62ffd83
reiko_iguchi <reiko6122@gmail.com>
2022-06-21 15:38:18 +0900

    2022/06/21

--- src/public/js/mypage.js
+++ src/public/js/mypage.js
@@ -1,4 +1,4 @@
-function changeNumberAndPrice(loop_index) {//人数を変更した場合、金額を変更
+function changeNumberAndPrice(loop_index) {//人数を変更した場合、金額も変更
   const reservation_number = document.getElementById('number' + loop_index)
   const selected_index = reservation_number.selectedIndex;
   const number = reservation_number.options[selected_index].value;
